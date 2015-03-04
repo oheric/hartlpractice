2fa045ee5f4edd24a078186431a9675ca600f695
oheric <ohericyoung@gmail.com>
2015-03-03 21:01:41 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hartlpractice::Application.config.secret_key_base = '9cc8a4fe5c9fafb7a15b899e0ad7452e23ed737c6e1405d19bcd5eff075eaf04975a688ee56e616b65ce866015521daeace8c38e00b41883d7169610a40b932a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hartlpractice::Application.config.session_store :cookie_store, key: '_hartlpractice_session'
